55eaba0238469be78dbf2fc53f7e007ab04d2bea
netevert <netevert@protonmail.com>
2020-03-03 21:15:32 +0100

    merge master

--- lab/files/create-ad.ps1
+++ lab/files/create-ad.ps1
@@ -1,6 +1,6 @@
 # Provision domain
 Import-Module ADDSDeployment
-$password = ConvertTo-SecureString "fDIUH68dkjwuhd52333" -AsPlainText -Force
+$password = ConvertTo-SecureString "{your_admin_password}" -AsPlainText -Force
 Add-WindowsFeature -name ad-domain-services -IncludeManagementTools
-Install-ADDSForest -CreateDnsDelegation:$false -DomainMode Win2012R2 -DomainName "helheim.local" -DomainNetbiosName "helheim" -ForestMode Win2012R2 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true
+Install-ADDSForest -CreateDnsDelegation:$false -DomainMode Win2012R2 -DomainName "{your_domain}" -DomainNetbiosName "{your_netbios_name}" -ForestMode Win2012R2 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true
 shutdown -r -t 10
